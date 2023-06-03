9a72e3170d37de1b48cfa9128ee779860f8f3e8f
Maaaaaaaaht <125898593+Maaaaaaaaht@users.noreply.github.com>
2023-06-03 16:53:26 +0100

    Add a navigation scroll function

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const buttonFunc = () => {
-    console.log('click')
-}
+const scrollFunction = (value) => {
+  if (value == "home") {
+    document
+      .getElementsByClassName("section1")[0]
+      .scrollIntoView({ behavior: "smooth" });
+  }
+  if (value == "about") {
+    var element = document.getElementsByClassName("section2")[0];
+    var elementPosition = element.getBoundingClientRect().top;
+    var headerOffset = 80;
+    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  }
+  if (value == "work") {
+    document
+      .getElementsByClassName("section3")[0]
+      .scrollIntoView({ behavior: "smooth" });
+  }
+};
